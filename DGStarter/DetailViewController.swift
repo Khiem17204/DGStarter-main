cd0f7b9b7e44d5348542d42946b8fdaecca5b94f
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2023-02-25 12:12:39 -0500

    Detail Page design added

--- DGStarter/DetailViewController.swift
+++ DGStarter/DetailViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    
+    var dinosaur: Dinosaur?
+    
+    @IBOutlet weak var DinosaurImage: UIImageView!
+    @IBOutlet weak var DinosaurName: UILabel!
+    
+    @IBOutlet weak var DinosaurType: UILabel!
+    
+    @IBOutlet weak var DinosaurWeight: UILabel!
+    @IBOutlet weak var DinosaurHeight: UILabel!
+    @IBOutlet weak var Diet: UILabel!
+    @IBOutlet weak var Era: UILabel!
+    @IBOutlet weak var Region: UILabel!
+    @IBOutlet weak var Speed: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let dinosaur = dinosaur {
+            DinosaurImage.image = dinosaur.image
+            DinosaurName.text = dinosaur.name
+            DinosaurType.text = dinosaur.type
+            DinosaurWeight.text = String(dinosaur.weight) + "KG"
+            DinosaurHeight.text = String(dinosaur.height) + "M"
+            Diet.text = dinosaur.diet
+            Era.text = dinosaur.era
+            Region.text = dinosaur.region
+            Speed.text = String(dinosaur.speed) + "mph"
+            }
         // Do any additional setup after loading the view.
     }
     
